5bcfbf7d51cc13ae216e19be82841314940452a3
Martin Fenner <martin.fenner@datacite.org>
2019-01-15 16:56:54 +0100

    show external identifiers

--- app/components/organization-item.js
+++ app/components/organization-item.js
@@ -1,15 +1,22 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  isni: null,
+  wikidata: null,
+
   didInsertElement() {
     this.set('aliases', this.model.get('aliases').join(', '));
     this.set('acronyms', this.model.get('acronyms').join(', '));
 
-    let external_ids = this.model.get('external_ids');
-    // .map(id => { 
-    //   item.id = item.id.substr(8);
-    //   return item;
-    // });
-    this.set('external_ids', external_ids);
+    this.set('grid', this.model.get('local'));
+    if (this.model.get('external_ids.ISNI')) {
+      this.set('isni', this.model.get('external_ids.ISNI').all);
+    }
+    if (this.model.get('external_ids.FundRef')) {
+      this.set('fundref', this.model.get('external_ids.FundRef').all);
+    }
+    if (this.model.get('external_ids.Wikidata')) {
+      this.set('wikidata', this.model.get('external_ids.Wikidata').all);
+    }
   }
 });
