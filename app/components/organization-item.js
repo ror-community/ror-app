8b39498a24dc20ed1ba6dfdeb294bae8d75131c5
Martin Fenner <martin.fenner@datacite.org>
2019-01-15 18:14:22 +0100

    adapt external ids display

--- app/components/organization-item.js
+++ app/components/organization-item.js
@@ -8,15 +8,18 @@ export default Component.extend({
     this.set('aliases', this.model.get('aliases').join(', '));
     this.set('acronyms', this.model.get('acronyms').join(', '));
 
-    this.set('grid', this.model.get('local'));
+    this.set('grid', this.model.get('external_ids.GRID').preferred);
     if (this.model.get('external_ids.ISNI')) {
-      this.set('isni', this.model.get('external_ids.ISNI').all);
+      let isni = this.model.get('external_ids.ISNI').all.get('firstObject').replace(/-|\s/g,"");
+      this.set('isni', isni);
     }
     if (this.model.get('external_ids.FundRef')) {
-      this.set('fundref', this.model.get('external_ids.FundRef').all);
+      let fundref = this.model.get('external_ids.FundRef').all.get('firstObject');
+      this.set('fundref', fundref);
     }
     if (this.model.get('external_ids.Wikidata')) {
-      this.set('wikidata', this.model.get('external_ids.Wikidata').all);
+      let wikidata = this.model.get('external_ids.Wikidata').all.get('firstObject');
+      this.set('wikidata', wikidata);
     }
   }
 });
