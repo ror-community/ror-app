2d0d70fc1ca1b338bca76454bce777113553e7ba
Martin Fenner <martin.fenner@datacite.org>
2019-01-15 19:26:30 +0100

    use unescaped path to ror id

---
+++ app/adapters/organization.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+import ENV from 'ror-app/config/environment';
+
+export default DS.JSONAPIAdapter.extend({
+  host: ENV.API_URL,
+
+  urlForFindRecord(id, modelName, snapshot) {
+    return `${ENV.API_URL}/${modelName}s/ror.org/${id}`;
+  }
+});
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -5,14 +5,14 @@ export default DS.JSONSerializer.extend({
   normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
     payload = payload.items.map(item => { 
       // strip "https://" from id
-      item.id = item.id.substr(8);
+      item.id = item.id.substr(16);
       return item;
    });
     return this._super(store, primaryModelClass, payload, id, requestType);
   },
   normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
     // strip "https://" from id
-    payload.id  = payload.id.substr(8);
+    payload.id  = payload.id.substr(16);
     return this._super(store, primaryModelClass, payload, id, requestType);
   },
   // normalizeFindRecordResponse(store, primaryModelClass, payload) {
