a205d17b225d1737745093ca8338b3130bac02db
Martin Fenner <martin.fenner@datacite.org>
2019-10-01 19:27:15 +0200

    Revert "handle api errors graciously. #11"
    This reverts commit dc085153f8d83523192bd6e2f08fdc5cd83d6bf9.

--- app/routes/organizations/index.js
+++ app/routes/organizations/index.js
@@ -2,12 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.store.query('organization', params).then(function(model) {
-      return model;
-    }).catch(function (reason) {
-      console.log(reason);
-      return [];
-    });
+    return this.store.query('organization', params);
   },
 
   queryParams: {
