9134f875e52f1b8103371048f8e00f78851851c2
lizkrznarich <liz.krznarich@datacite.org>
2022-05-19 13:23:15 -0500

    skip linting regex line

--- app/routes/organizations/index.js
+++ app/routes/organizations/index.js
@@ -4,8 +4,8 @@ export default Route.extend({
   model(params) {
     /*Escape Elasticsearch reserved chars
     https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#_reserved_characters*/
-    // eslint-disable-next-line
     if(params.query){
+      // eslint-disable-next-line
       params.query = params.query.replace(/([\+\-\=\&\|\>\<\!\(\)\{\}\\\[\]\^\"\~\*\?\:\/])/g, "\\$1");
     }
     return this.store.query('organization', params).then(function(model) {
