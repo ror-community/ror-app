d4881afd2c5b1b538ede29a56715beab4d9abdbe
lizkrznarich <liz.krznarich@datacite.org>
2022-05-19 10:25:48 -0500

    escape ES reserved chars in UI search

--- app/routes/organizations/index.js
+++ app/routes/organizations/index.js
@@ -2,6 +2,10 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
+    /*Escape Elasticsearch reserved chars
+    https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#_reserved_characters*/
+    // eslint-disable-next-line
+    params.query = params.query.replace(/([\+\-\=\&\|\>\<\!\(\)\{\}\\\[\]\^\"\~\*\?\:\/])/g, "\\$1");
     return this.store.query('organization', params).then(function(model) {
       return model;
     }).catch(function (reason) {
