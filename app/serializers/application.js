95df5ed7b91fc93b6ef16d1e395f8c009ba62fce
Adam Buttrick <adambuttrick@Adams-MacBook-Pro-2.local>
2022-06-30 11:52:28 -0700

    Pushing Adam's prototype of displaying relationshionship

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -2,6 +2,20 @@ import { underscore } from '@ember/string';
 import { assign } from '@ember/polyfills';
 import DS from 'ember-data';
 
+// Convert label array into a dictionary with relationship type as key
+// for variable fields in template
+function convertReationships(relationships){
+  if(Array.isArray(relationships) && !relationships.length){
+    return relationships;
+  } else {
+      let formattedRelationships = {"Parent":[], "Child":[], "Related":[]};
+      for (let i = 0; i < relationships.length; i++){
+        formattedRelationships[relationships[i]["type"]].push({"label": relationships[i]["label"], "id": relationships[i]["id"]});
+      }
+      return formattedRelationships
+    }
+  }
+
 export default DS.JSONSerializer.extend({
   normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
     let total = payload.number_of_results;
@@ -10,6 +24,7 @@ export default DS.JSONSerializer.extend({
     payload = payload.items.map(item => { 
       // strip "https://" from id
       item.id = item.id.substr(16);
+      item.relationships = convertReationships(item.relationships);
       return item;
     });
     let data = this._super(store, primaryModelClass, payload, id, requestType);
@@ -18,6 +33,7 @@ export default DS.JSONSerializer.extend({
   normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
     // strip "https://" from id
     payload.id  = payload.id.substr(16);
+    payload.relationships = convertReationships(payload.relationships);
     return this._super(store, primaryModelClass, payload, id, requestType);
   },
   // normalizeFindRecordResponse(store, primaryModelClass, payload) {
@@ -25,7 +41,7 @@ export default DS.JSONSerializer.extend({
 
   //   return this._super(store, primaryModelClass, payload);
   // },
-  keyForAttribute(attr) {
+    keyForAttribute(attr) {
     return underscore(attr);
   }
 });
