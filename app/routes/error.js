c15897ddcae2ea157ac8e377dd7153dc82e161db
lizkrznarich <liz.krznarich@datacite.org>
2023-03-27 10:11:35 -0500

    redirect 404 to info site and update fallback template text

--- app/routes/error.js
+++ app/routes/error.js
@@ -4,5 +4,5 @@ import ENV from 'ror-app/config/environment';
 export default Route.extend({
     redirect: function() {
         window.location.replace(ENV.BASE_URL + "/error");
-     }
+    }
 });
