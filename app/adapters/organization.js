88a8b0ea90ccd25d844f63588301d0596e714ec8
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-02-05 12:57:03 +0200

    Update the adapters to use remote flags

--- app/adapters/organization.js
+++ app/adapters/organization.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
 import ENV from 'ror-app/config/environment';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default DS.JSONAPIAdapter.extend({
-  host: window.ld.variation("v2") ? ENV.API_URL_V2 : ENV.API_URL_V1,
+  launchDarkly: service(),
+  host: computed('launchDarkly.variation', function() {
+    return this.launchDarkly.variation('v2_ui') ? ENV.API_URL_V2 : ENV.API_URL_V1;
+  }),
 
   urlForFindRecord(id, modelName) {
     return `${this.host}/${modelName}s/ror.org/${id}`;
