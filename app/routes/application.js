6ad445049e768f996b5105c8948c61025b4dc217
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-02-05 12:58:45 +0200

    Update the route to use remote flags

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
+  beforeModel(){
     let user = {
+      key: 'user-key-123abc',
       anonymous: true
     };
-
     return this.launchDarkly.initialize(user);
   }
 });
