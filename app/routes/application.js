5b45f74b78b34f4e6c01a13c4e8e7284b8c34b4c
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2024-02-19 21:07:32 +0200

    Revert "Support data in v2 (#194)"
    This reverts commit 0d951433cf2c3cd0a1c7335cc7ae524aefe1161e.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  beforeModel(){
-    let user = {
-      key: 'user-key-123abc',
-      anonymous: true
-    };
-    return this.launchDarkly.initialize(user);
-  }
+
 });
