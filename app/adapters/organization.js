4e98d4abac88493b7682d96edbb92f622942269e
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2024-03-04 18:46:54 +0200

    Revert "Support for schema v2 (#198)"
    This reverts commit eef6b0a72340dd6749269c83029a596e7a045381.

--- app/adapters/organization.js
+++ app/adapters/organization.js
@@ -1,16 +1,11 @@
 import DS from 'ember-data';
 import ENV from 'ror-app/config/environment';
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 
 export default DS.JSONAPIAdapter.extend({
-  launchDarkly: service(),
-  host: computed('launchDarkly.variation', function() {
-    return this.launchDarkly.variation('v2_ui') ? ENV.API_URL_V2 : ENV.API_URL_V1;
-  }),
+  host: ENV.API_URL,
 
   urlForFindRecord(id, modelName) {
-    return `${this.host}/${modelName}s/ror.org/${id}`;
+    return `${ENV.API_URL}/${modelName}s/ror.org/${id}`;
   },
 
   init() {
--- config/dotenv.js
+++ config/dotenv.js
@@ -1,6 +1,6 @@
 module.exports = function(env) {
   return {
-    clientAllowedKeys: ['API_URL_V1', 'API_URL_V2'],
+    clientAllowedKeys: ['API_URL'],
     failOnMissingKey: false
   };
 };
