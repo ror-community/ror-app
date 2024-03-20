6bef291fb12698df5f79eefe489fed53eb5d0a80
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-03-20 13:38:30 +0200

    Initial upgrade to 3.28

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,8 +29,27 @@ module.exports = {
       },
       env: {
         browser: false,
+<<<<<<< HEAD
         node: true
       }
     }
   ]
+=======
+        node: true,
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+      rules: {
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off',
+      },
+    },
+    {
+      // Test files:
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+    },
+  ],
+>>>>>>> 9c76a415 (v4.9.2...v3.28.6)
 };
