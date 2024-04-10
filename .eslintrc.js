9b0ab95bb934e596e11de509e66b11d39c0e7469
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-04-10 16:59:29 +0200

    Revert changes to the eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,19 +30,7 @@ module.exports = {
       env: {
         browser: false,
         node: true,
-      },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
-      rules: {
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off',
-      },
-    },
-    {
-      // Test files:
-      files: ['tests/**/*-test.{js,ts}'],
-      extends: ['plugin:qunit/recommended'],
-    },
-  ],
+      }
+    }
+  ]
 };
