c3b8b44b8566e24afcd6d8bc24f127aebdeac149
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-04-10 17:11:48 +0200

    Remove jquery and update truth helper

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import ENV from 'ror-app/config/environment';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 
-export default DS.JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend({
   launchDarkly: service(),
   host: computed('launchDarkly.variation', function() {
     return this.launchDarkly.variation('v2_ui') ? ENV.API_URL_V2 : ENV.API_URL_V1;
