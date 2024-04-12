3db61ba4709b37b2ff58d7fb32d6c3a7331e16fb
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-04-12 15:48:44 +0200

    Add lang to all otherNames

--- app/components/v2/organization-item.js
+++ app/components/v2/organization-item.js
@@ -45,12 +45,14 @@ export default Component.extend({
       return result;
     }, {});
 
-    if (groupedNames.label) {
-      groupedNames.label.forEach((label) => {
-        if (label.lang && !label.value.includes(`(${label.lang})`)) {
-          label.value = `${label.value} (${label.lang})`;
-        }
-      });
+    for (let key in groupedNames) {
+      if (groupedNames[key]) {
+        groupedNames[key].forEach((item) => {
+          if (item.lang && !item.value.includes(`(${item.lang})`)) {
+            item.value = `${item.value} (${item.lang})`;
+          }
+        });
+      }
     }
 
     const values = Object.values(groupedNames).flat().map(item => item.value).join(', ');
