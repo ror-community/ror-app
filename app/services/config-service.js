c58f6d6ad9b9bd3b22143550bfeb107dc9dbed3f
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2024-04-12 16:22:04 +0200

    Revert "[Feature | BUG] - Handle lang attribute for all Othernames (#207)"
    This reverts commit a0c533a31c29c7ee933bbb0680d3547d1e363fda.

--- app/services/config-service.js
+++
@@ -1,7 +0,0 @@
-import Service from '@ember/service';
-import config from '../config/environment';
-
-export default Service.extend({
-  API_URL_V1: config.API_URL_V1,
-  API_URL_V2: config.API_URL_V2
-});
