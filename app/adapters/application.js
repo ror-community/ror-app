5aab7482da0fd532d7d6138ec3bd522e05bdfdef
Kudakwashe Siziva <kuda.siziva@datacite.org>
2024-08-09 16:58:55 +0200

    Remove launch darkly and v1 support

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,6 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import ENV from 'ror-app/config/environment';
-import { variation } from 'ember-launch-darkly';
 
 export default JSONAPIAdapter.extend({
-  host: variation('v2_ui') ? ENV.API_URL_V2 : ENV.API_URL_V1
+  host: ENV.API_URL
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,30 +1,4 @@
 import Route from '@ember/routing/route';
-import { initialize } from 'ember-launch-darkly';
-import config from '../config/environment';
 
 export default class ApplicationRoute extends Route {
-  async model() {
-    const user = {
-      key: 'user-key-123abc',
-      anonymous: true
-    };
-
-    // Destructure clientSideId and any other config options
-    const { launchDarkly: { clientSideId }, ...restConfig } = config;
-    
-    console.log('clientSideId:', clientSideId);
-    console.log('user:', user);
-    console.log('config rest:', restConfig);
-
-    try {
-      // Initialize LaunchDarkly client and wait for it to complete
-      const result = await initialize(clientSideId, user, restConfig);
-      console.log('LaunchDarkly initialized:', initialize());
-      return result;
-    } catch (error) {
-      console.error('Error initializing LaunchDarkly:', error);
-      // Handle initialization error if needed, e.g., fallback logic or empty model
-      return {}; // or handle it according to your app's needs
-    }
-  }
 }
