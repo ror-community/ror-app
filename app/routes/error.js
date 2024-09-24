a2bf037cd1982ad2ae67c6fa9bd51a69ceaa2d11
kaysiz <ksiziva@gmail.com>
2024-09-24 14:05:51 +0200

    Clean slate

--- app/routes/error.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-import ENV from 'ror-app/config/environment';
-
-export default Route.extend({
-    redirect: function() {
-        window.location.replace(ENV.BASE_URL + "/error");
-    }
-});
--- config/dotenv.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(env) {
-  return {
-    clientAllowedKeys: ['API_URL_V1', 'API_URL_V2'],
-    failOnMissingKey: false
-  };
-};
